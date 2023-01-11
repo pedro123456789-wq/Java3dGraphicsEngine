9127aa7aeb21117a1bfade459862c950d43a372e
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2023-01-11 21:15:49 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,25 @@
-import java.awt.Toolkit;
-
 import javax.swing.JFrame;
+
+
+//TODO:
+//-> Fix bubble sort for edge creation
+//-> Carry on with video 13 by adding rotation vector 
+
 
 public class Main extends JFrame {
 	private static final long serialVersionUID = 1L;
 	static JFrame frame = new Main();
 	Screen screenObj = new Screen();
-	
+
 	public Main() {
 		add(screenObj);
-		setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		setSize(Constants.screenSize);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("3D Graphics Engine");
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
 
 	}
-
 }
