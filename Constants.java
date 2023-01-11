9d39646dbb7ca8af38a974974d6c43a3496b0833
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2023-01-11 21:15:25 +0000

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,7 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class Constants {
+	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	static int FPS = 30;		
+}
